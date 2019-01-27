fe0342d88eab0a3df80af71239c3ea67fa5932b1
1875167603 <1875167603@qq.com>
2019-01-27 20:03:02 +0800

    by dty717

--- src/main/java/WebSite/src/test/Testjsoup.java
+++ src/main/java/WebSite/src/test/Testjsoup.java
@@ -1,60 +1,16 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-
-
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
 import java.io.IOException;
 
 public class Testjsoup {
     public static void main(String[] args) throws IOException {
-        //Connection  con=Jsoup.connect("https://www.google.com?q=Hello+World");
-        //Connection.Response res=con.method(Connection.Method.GET).execute();
-        System.out.print(google_results("Hello",12));
+        Connection  con=Jsoup.connect("http://www.google.com?q=Hello+World");
+        Connection.Response res=con.method(Connection.Method.GET).execute();
+        System.out.print(res.body());
     }
-    public static List<Map<String,String>> google_results(String keyword, int no_of_results) throws IOException
-	{
-		List<Map<String,String>> gresults = new ArrayList<Map<String,String>>(no_of_results);
-		
-		//Replace space by + in the keyword as in the google search url
-		keyword = keyword.replace(" ", "+");
-		
-		String url = "https://www.google.co.in/search?q=" + keyword + "&num=" + String.valueOf(no_of_results);
-	
-		//Connect to the url and obain HTML response
-		Document doc = Jsoup
-				.connect(url)
-				.userAgent("Mozilla")
-				.timeout(5000).get();
-		
-	
-		//parsing HTML after examining DOM
-		Elements els  = doc.select("li.g");
-		
-		for(Element el : els)
-		{
-			Map<String, String> map = new HashMap<String, String>();
-			
-			map.put("title", el.getElementsByTag("h3").text());
-			map.put("site", el.getElementsByTag("cite").text());
-			map.put("abstract", el.getElementsByTag("span").text());
-			gresults.add(map);
-			
-			//Print title, site and abstract
-			//System.out.println("Title : " + el.getElementsByTag("h3").text());
-			//System.out.println("Site : " +  el.getElementsByTag("cite").text());
-			//System.out.println("Abstract : " + el.getElementsByTag("span").text() + "\n");
-		}
 
-		
-		return gresults;
-	}
-    
 }
