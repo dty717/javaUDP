64298952c2003daf686852eb7d6ea280a85f6247
1875167603 <1875167603@qq.com>
2019-01-27 20:06:47 +0800

    by dty717

--- src/main/java/WebSite/src/test/Testjsoup.java
+++ src/main/java/WebSite/src/test/Testjsoup.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class Testjsoup {
     public static void main(String[] args) throws IOException {
-        Connection  con=Jsoup.connect("http://www.google.com?q=Hello+World");
+        Connection  con=Jsoup.connect("https://www.google.com/search?hl=en&ei=6VxMXOWqK56t0PEP_N2fqAo&q=detect+qrcode&oq=detect+qrcode");
         Connection.Response res=con.method(Connection.Method.GET).execute();
         System.out.print(res.body());
     }
