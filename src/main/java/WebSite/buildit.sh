8b202fee79f894c12145f1306167f5959399ee06
1875167603 <1875167603@qq.com>
2019-01-27 19:52:58 +0800

    build.sh

--- src/main/java/WebSite/buildit.sh
+++ src/main/java/WebSite/buildit.sh
@@ -3,4 +3,4 @@ cd /usr/web//javaUDP/src/main/java/WebSite/src/test/
 javac -cp "/usr/web//javaUDP/src/main/java/WebSite/out/lib/jsoup-1.8.1.jar":/usr/web/javaUDP/src/main/java/WebSite/src/test/ -d /usr/web/javaUDP/src/main/java/WebSite/out Testjsoup.java
 cd ~
 cd /usr/web/javaUDP/src/main/java/WebSite/src/
-java -cp /usr/web/javaUDP/src/main/java/WebSite/out:"/usr/web/javaUDP/src/main/java/WebSite/out/lib/jsoup-1.8.1.jar" test.Testjsoup
+java -cp /usr/web/javaUDP/src/main/java/WebSite/out:"/usr/web/javaUDP/src/main/java/WebSite/out/lib/jsoup-1.8.1.jar" test.Testjsoup > /usr/web/javaUDP/src/main/java/WebSite/out/test.html
