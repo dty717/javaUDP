ddf91c5875df0e53969e77340ec9e40a8269ff5e
1875167603 <1875167603@qq.com>
2019-02-18 13:30:32 +0800

    gbk

--- src/main/java/UDPConnection/GoogleApi.java
+++ src/main/java/UDPConnection/GoogleApi.java
@@ -117,7 +117,7 @@ public class GoogleApi extends Thread{
         URLConnection connection = new URL("https://www.google.com/search?q=" + query).openConnection();
         connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
         connection.connect();
-
+        
         BufferedReader r  = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")));
 
         StringBuilder sb = new StringBuilder();
@@ -132,6 +132,8 @@ public class GoogleApi extends Thread{
         String link = "error";
         if(start!=-1&&end!=-1)
             link=sb.toString().substring(start+8,end);
+        System.out.println(sb.toString());
+        
         return link;
     }
 }
--- src/main/java/UDPConnection/runJava.bat
+++ src/main/java/UDPConnection/runJava.bat
@@ -1,5 +1,5 @@
 cd C:/Users/xqy/Desktop/github/javaUDP/src/main/java/UDPConnection/
 javac -cp C:/Users/xqy/Desktop/github/javaUDP/src/main/java/UDPConnection/ -d out GetGoogle.java 2> error_test.log
-java -cp C:/Users/xqy/Desktop/github/javaUDP/src/main/java/UDPConnection/out  UDPConnection.GetGoogle > test.txt
+java -cp C:/Users/xqy/Desktop/github/javaUDP/src/main/java/UDPConnection/out  UDPConnection.GetGoogle
 pause
 rem GetGoogle GoogleApi
